112ddf6f73bd2888dbae416e0f74f97e75be7414
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-03 16:25:39 +0200

    Update sales_targets.py

--- Sales_Targets/sales_targets.py
+++ Sales_Targets/sales_targets.py
@@ -1,1 +1,8 @@
-print('Hello, world')
+# Author: Tharcisio Leone #
+# Dataset: Targets of Sales #
+
+## Maschine Learning using openpyxl and twilio
+# 0. Importing Libraries.
+# 1. Opening the six files in Excel.
+# 2. Evaluating the values of sales
+# --> If the value is higher than 55.000, then send SMS reporting the month, salesperson and value.
