06b6a2794f3824f7206df3f5544653028a6f7c1f
Tharcisio Leone <107752043+tharcisioleone@users.noreply.github.com>
2022-07-03 17:46:24 +0200

    Update sales_targets.py

--- Sales_Targets/sales_targets.py
+++ Sales_Targets/sales_targets.py
@@ -13,20 +13,6 @@ import pandas as pd
 from twilio.rest import Client
 
 
-# 1. Opening the six files in Excel containing the sales values per salesperson.
-list_months = ['january', 'february', 'march', 'april', 'may', 'june']
-
-for months in list_months:
-    #print(months)
-    table_sales = pd.read_excel(f'{months}.xlsx')
-    #print(table_sales)
-    if (table_sales['Values'] > 55000).any(): # 2. Evaluating the values of sales
-        salesperson = table_sales.loc[table_sales['Values'] > 55000, 'Salesperson'].values[0]
-        values = table_sales.loc[table_sales['Values'] > 55000, 'Values'].values[0]
-        print(f'In month {months} the target was achieved. Salesperson: {salesperson}. Value: {values}.')
-
-
-# 3. Sending SMS with the message of target achieving reporting the month, salesperson and value.
 # See: https://www.twilio.com/docs/libraries/python
 
 # Copy and paste from homepage
@@ -37,6 +23,26 @@ account_sid = "AC2a465d6668cd63f1d30b7ac11f206aa8"
 # Your Auth Token from twilio.com/console
 auth_token  = "25ce7ce97055f1f5e21af2e4e1bda957"
 client = Client(account_sid, auth_token)
+
+
+# 1. Opening the six files in Excel containing the sales values per salesperson.
+list_months = ['january', 'february', 'march', 'april', 'may', 'june']
+
+for months in list_months:
+    #print(months)
+    table_sales = pd.read_excel(f'{months}.xlsx')
+    #print(table_sales)
+    if (table_sales['Values'] > 55000).any(): # 2. Evaluating the values of sales
+        salesperson = table_sales.loc[table_sales['Values'] > 55000, 'Salesperson'].values[0]
+        values = table_sales.loc[table_sales['Values'] > 55000, 'Values'].values[0]
+        message = client.messages.create( # 3. Sending SMS with the message of target achieving reporting the month, salesperson and value.
+            to="+4917681703460",
+            from_="+19707143233",
+            body=f'In month {months} the target was achieved. Salesperson: {salesperson}. Value: {values}.')
+        print(message.sid)
+
+# 3. Sending SMS with the message of target achieving reporting the month, salesperson and value.
+
 
 message = client.messages.create(
     to="+4917681703460",
